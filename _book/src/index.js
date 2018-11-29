9cf44cccfaf64d23e94e77f9c4a352aef979fbbc
zyfzhang <497956511@qq.com>
2018-11-29 12:29:56 +0800

    update

---
+++ _book/src/index.js
@@ -0,0 +1,13 @@
+class FastCache{
+    constructor(){
+        this.list =  {}
+    }
+    set(key,value){
+        this.list[key] = value
+    }
+    get(key){
+        return this.list[key]
+    }
+}
+
+window.FastCache =  FastCache
